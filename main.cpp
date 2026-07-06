fa8c232366853c2f6b3c07f211ee2d3b266594a7
sadeem <86182888+nglint@users.noreply.github.com>
2026-07-06 08:30:04 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,129 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <cstdio>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+bool waitForData(int sock, int timeoutMs) {
+    fd_set readSet;
+    FD_ZERO(&readSet);
+    FD_SET(sock, &readSet);
+
+    timeval timeout;
+    timeout.tv_sec = timeoutMs / 1000;
+    timeout.tv_usec = (timeoutMs % 1000) * 1000;
+
+    // Linux requires the first argument to be the highest fd + 1
+    int result = select(sock + 1, &readSet, nullptr, nullptr, &timeout);
+    return result > 0;
+}
+
+int main() {
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8888);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("bind");
+        close(sock);
+        return 1;
+    }
+
+    char *buffer = (char*)malloc(10485760 * sizeof(char));
+    char *flag = (char*)malloc(10485760 * sizeof(char));
+    memset(flag, 0, 10485760);
+
+    int recvBufferSize = 100 * 1024 * 1024;
+    setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &recvBufferSize, sizeof(recvBufferSize));
+
+    char packet[1472];
+    unsigned short *seq = (unsigned short*)packet;
+    struct sockaddr_in clientAddr;
+    socklen_t addrLen = sizeof(clientAddr);
+    int c = 0, fi = 0;
+    int bytesAvailable = 0;
+    bool f = true;
+    short ma = 0, m[4], u = 0;
+
+    // Wait until data is available (Linux uses ioctl with FIONREAD)
+    while (bytesAvailable == 0) {
+        ioctl(sock, FIONREAD, &bytesAvailable);
+    }
+
+    auto s = chrono::steady_clock::now();
+    for (;;) {
+        if (waitForData(sock, 100)) {
+            int bytes = recvfrom(sock, packet, 1452, 0,
+                                 (struct sockaddr*)&clientAddr, &addrLen);
+            if (bytes < 1) continue;
+
+            if (seq[0] > ma || (abs(seq[0] - ma) > 2000 && ma > seq[0]))
+                ma = seq[0];
+
+            memcpy(buffer + (seq[0] * 1450), packet + 2, 1450);
+            flag[seq[0]] = 10;
+            c++;
+            int g = 0;
+            if (ma > 3000) {
+                f = false;
+            }
+
+            for (int i = 10, l = 1; i < 1800 && g * 2 < 1452; i++) {
+                if (i % 600 == 0) l++;
+                int p = ma - i;
+                if (p < 0) {
+                    if (f == 0) p += 6888;
+                    else break;
+                }
+                if (flag[p] < l) {
+                    flag[p] = l;
+                    memcpy(&packet[g * 2], &p, sizeof(short));
+                    g++;
+                }
+            }
+
+            auto e = chrono::steady_clock::now();
+            if (g != 0) {
+                sendto(sock, packet, g * 2, 0,
+                       (struct sockaddr*)&clientAddr, sizeof(clientAddr));
+                fi += g;
+            }
+
+            if (chrono::duration_cast<chrono::microseconds>(e - s).count() >= 1000000) {
+                cout << "lose: " << fi << " rec:" << c << "\n";
+                c = 0;
+                fi = 0;
+                s = chrono::steady_clock::now();
+            }
+            u++;
+            if (f == 0) {
+                int p = ma - 3000;
+                if (p < 0) p += 6888;
+                for (int i = p, n = 0; n < 3000; n++, i--) {
+                    if (i < 0) i += 6888;
+                    flag[i] = 0;
+                }
+            }
+        }
+    }
+
+    close(sock);
+    free(buffer);
+    free(flag);
+    return 0;
+}
